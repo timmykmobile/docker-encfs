f33187cd00319ff47b2052722b0d4f7fbe2e06b2
timmykmobile <40679623+timmykmobile@users.noreply.github.com>
2020-04-18 19:59:14 +1000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "hello!"
+echo "$ENCFS_REVERSE"
+echo "$ENCFS_XML_NAME"
+echo "$ENCFS_PASSWORD_NAME"
 
 # Making the /config directory
 MOUNT_SOURCE=mount-source
@@ -14,8 +18,6 @@ if [ -z "$(getent group abc)" ]; then
         echo "creating abc group for gid ${PGID}"
         groupadd -g ${PGID} -o abc >/dev/null 2>&1
 fi
-
-
 
 # Create a user for our uid if required
 if [ -z "$(getent passwd abc)" ]; then
@@ -59,20 +61,29 @@ fi
 
 # Check if existing mount
 if grep -qs '/$MOUNT_TARGET ' /proc/mounts; then
-    echo "It's mounted."
+    echo "Something is already mounted at $MOUNT_TARGET"
 else
-    echo "It's not mounted."
-
     # Mount away!
-    #if [ "$ENCFS_PASS" ]; then
-        echo "Mounting at target"
-        ENCFS6_CONFIG='/config/encfs.xml' encfs --extpass="cat /config/encfspass" -o allow_other /$MOUNT_SOURCE /$MOUNT_TARGET
-        echo "Mounted now hopefully"
-    #else
-    #    encfs -o allow_other $MOUNT_SOURCE $MOUNT_TARGET
-    #fi
+    if [ "$ENCFS_REVERSE" -eq "0" ]; then
+        echo "Reverse is set to no, thus standard mount.  Ready to mount now."
+        ENCFS6_CONFIG='/config/'$ENCFS_XML_NAME encfs --extpass="cat /config/$ENCFS_PASSWORD_NAME" -o allow_other /$MOUNT_SOURCE /$MOUNT_TARGET
+        if grep -qs '/$MOUNT_TARGET ' /proc/mounts; then
+                echo "Successfully mounted at $MOUNT_TARGET"
+        fi
+    else
+        echo "Ready to set mount as reverse..."
+#        encfs -o allow_other $MOUNT_SOURCE $MOUNT_TARGET
+        encfs --extpass="cat /config/$ENCFS_PASSWORD_NAME" --reverse -o allow_other "/$MOUNT_SOURCE" "/$MOUNT_TARGET"
+            if grep -qs '/$MOUNT_TARGET ' /proc/mounts; then
+                echo "Successfully mounted at $MOUNT_TARGET"
+            fi
 
 fi
+fi
+
+
+#trap 'umount -f /"$MOUNT_TARGET"' SIGTERM
+trap 'fusermount -u -z "$MOUNT_TARGET"' SIGTERM
 
 # and below to keep the container running.
 tail -f /dev/null
